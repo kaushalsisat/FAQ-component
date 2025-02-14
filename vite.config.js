890b46e639c0a90e804e356e71630a136df517c7
kaushalsisat@gmail.com <kaushalsisat@gmail.com>
2025-02-14 14:29:23 +0530

    by ks

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: "build", // This makes sure Vite outputs to the 'build' folder
-  },
-  base: "/FAQ-component/", // Add this line
 })
